deedfeddb1790a8289c8fc5cf3cbbaf0c70c0f29
Guillem Forns Sant <gforns-s@car3s4.42barcelona.com>
2023-09-04 12:47:06 +0200

    cleared all test and starting to program algorithm

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 12:34:33 by gforns-s          #+#    #+#             */
-/*   Updated: 2023/09/04 12:11:39 by gforns-s         ###   ########.fr       */
+/*   Updated: 2023/09/04 12:45:19 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int	main(int argc, char **argv)
 	t_stack	*a;
 	t_stack	*b;
 	char	**matrix;
-	int		z;
 
 	a = NULL;
 	b = NULL;
@@ -30,68 +29,9 @@ int	main(int argc, char **argv)
 	{
 		matrix = ft_check_args(argc, argv);
 		a = fill_stack(matrix);
-		b = fill_stack(matrix);
-		z = repited_value_check(a);
-		ft_printf("---Print A---\n");
-		print_nodes(a);
-		ft_printf("---Print B----\n");
-		print_nodes(b);
-		ft_printf("\n");
-		ft_printf("\n");
-		// /*next 2 lines are test*/
-		// ft_printf("--PB--\n");
-		// push_b(&b, &a);
-		// ft_printf("----A----\n");
-		// print_nodes(a);
-		// ft_printf("----B-----\n");
-		// print_nodes(b);
-		// ft_printf("\n");
-		// ft_printf("\n");
-		// /*next 2 lines are test*/
-		// ft_printf("--SA--\n");
-		// a = swap_a(a);
-		// ft_printf("----A----\n");
-		// print_nodes(a);
-		// ft_printf("----B-----\n");
-		// print_nodes(b);
-		// ft_printf("\n");
-		// ft_printf("\n");
-		// 		/*next 2 lines are test*/
-		// ft_printf("--RA--\n");
-		// a = rotate_a(a);
-		// ft_printf("----A----\n");
-		// print_nodes(a);
-		// ft_printf("----B-----\n");
-		// print_nodes(b);
-		// ft_printf("\n");
-		// ft_printf("\n");
-		// 				/*next 2 lines are test*/
-		// ft_printf("--RR--\n");
-		// rotate_rotate(&a, &b);
-		// ft_printf("----A----\n");
-		// print_nodes(a);
-		// ft_printf("----B-----\n");
-		// print_nodes(b);
-		// ft_printf("\n");
-		// ft_printf("\n");
-								/*next 2 lines are test*/
-		ft_printf("--RRA--\n");
-		a = reverse_rotate_a(a);
-		ft_printf("----A----\n");
-		print_nodes(a);
-		ft_printf("----B-----\n");
-		print_nodes(b);
-		ft_printf("\n");
-		ft_printf("\n");
-								/*next 2 lines are test*/
-		ft_printf("--RRR--\n");
-		reverse_rotate_rotate(&a, &b);
-		ft_printf("----A----\n");
-		print_nodes(a);
-		ft_printf("----B-----\n");
-		print_nodes(b);
-		ft_printf("\n");
-		ft_printf("\n");
+		if (repited_value_check(a) == 1)
+			return (write(2, "ERROR\n", 6));
+
 	}
 	return (0);
 }
